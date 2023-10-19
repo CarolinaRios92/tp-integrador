7f4d980bbace8752d47c5593b35546038b37fc88
Leandra Carolina Ríos <leandra.carolina.rios92@gmail.com>
2023-10-19 12:12:35 -0300

    javascript

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#form");
+const resetButton = document.querySelector("#reset-button");
+const resumenButton = document.querySelector("#resumen-button");
+const inputCantidad = document.querySelector("#cantidad");
+const categoria = document.querySelector("#categoria");
+const total = document.querySelector("#total");
+
+
+resetButton.addEventListener("click", function(e){
+    e.preventDefault()
+    form.reset();
+});
+
+resumenButton.addEventListener("click", function(e){
+    e.preventDefault();
+    total.innerHTML = "Total a Pagar: $";
+    const cantidad = inputCantidad.value;
+    let result = cantidad * 200;
+
+    if(categoria.value === "Estudiante"){
+        result = result * 0.2;
+        total.innerHTML += result;
+    } else if(categoria.value === "Trainee"){
+        result = result * 0.5;
+        total.innerHTML += result;
+    } else if(categoria.value === "Junior"){
+        result = result * 0.85;
+        total.innerHTML += result;
+    }
+})
